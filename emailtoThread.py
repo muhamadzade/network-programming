4245cd4e4f2dd0eb665932eec4e63a4ae87c71bd
muhamadzade <65562222+muhamadzade@users.noreply.github.com>
2021-08-13 10:23:33 +0430

    Update emailtoThread.py

--- emailtoThread.py
+++ emailtoThread.py
@@ -4,25 +4,8 @@ from email.mime.text import MIMEText
 
 from threading import Thread
 
-# fromaddr='infinity.xd85@gmail.com'
-# ##fromaddr='jmohmadz@alumni.ut.ac.ir'
-# toLs=['muhamadzade@gmail.com','amirkasrakaka@gmail.com','infinity.xd85@gmail.com']
-# ##to='muhamadzade@gmail.com'
-# ##to='j.mohammadzadeh@kiau.ac.ir'
-# for to in toLs:
-#     msg=MIMEMultipart()
-#     msg['From']=fromaddr
-#     msg['To']=to
-#     msg['Subject']='test subject'
-#     body='This is just a message'
-#     msg.attach(MIMEText(body,'plain'))
-#     server=smtplib.SMTP('smtp.gmail.com:587')
-#     server.starttls()
-#     server.login(fromaddr,'kiakasraarash321')
-#     text=msg.as_string()
-#     server.sendmail(fromaddr,to,text)
-#     print('Email send')
-# server.quit()
+
+
 class EmailThread(Thread):
     def __init__(self, email_to):
         self.email_to = email_to
